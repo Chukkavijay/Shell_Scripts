7531bc47be2a7346820b48053f250f9a21683e8c
Chukkavijay <113999369+Chukkavijay@users.noreply.github.com>
2024-04-05 12:24:20 +0530

    Update Age_Calculate.sh

--- Age_Calculate.sh
+++ Age_Calculate.sh
@@ -14,16 +14,3 @@ if [ $STATUS -eq 0 ]; then
 else
     echo "Hello $NAME, Your Age Is $CURENT_AGE and You Cannot Vote As Per Kejriwal"
 fi
-
-
-
-=================================================================
-helm repo add gitlab https://charts.gitlab.io
-helm repo update
-helm upgrade --install k8-connection gitlab/gitlab-agent \
-    --namespace gitlab-agent-k8-connection \
-    --create-namespace \
-    --set image.tag=v16.11.0-rc2 \
-    --set config.token=glagent-TxpiPHdzrLZQ_cEtysFdi_k8PEqZk58LbFzsN2VZdoy1qsm8gw \
-    --set config.kasAddress=wss://kas.gitlab.com
-==================================================================
